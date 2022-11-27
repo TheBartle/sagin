0cbcde6bea20c2d30c034cd8c0d79e67eb6761b9
Bartosz Lemieszko <bartosz.lemieszko@gmail.com>
2022-11-27 14:21:46 +0100

    lswsgi

---
+++ scripts/lswsgi.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+
+apt-get install build-essential wget python3-dev
+
+wget https://www.litespeedtech.com/packages/lsapi/wsgi-lsapi-2.1.tgz
+
+tar -xvf wsgi-lsapi-2.1.tgz
+
+cd wsgi-lsapi
+
+python3 ./configure.py
+make -j4 -l4
+cp lswsgi /usr/local/lsws/fcgi-bin/
+
+cd ..
+rm -rf wsgi-lsapi
+
+echo "completed"
