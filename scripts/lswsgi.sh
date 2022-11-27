a4d216e1c6f4089895ec119f307f0cbaaac6a4dc
Bartosz Lemieszko <bartosz.lemieszko@gmail.com>
2022-11-27 14:23:12 +0100

    lswsgi

--- scripts/lswsgi.sh
+++ scripts/lswsgi.sh
@@ -6,13 +6,13 @@ wget https://www.litespeedtech.com/packages/lsapi/wsgi-lsapi-2.1.tgz
 
 tar -xvf wsgi-lsapi-2.1.tgz
 
-cd wsgi-lsapi
+cd wsgi-lsapi-2.1
 
 python3 ./configure.py
 make -j4 -l4
 cp lswsgi /usr/local/lsws/fcgi-bin/
 
 cd ..
-rm -rf wsgi-lsapi
+rm -rf wsgi-lsapi-2.1
 
 echo "completed"
